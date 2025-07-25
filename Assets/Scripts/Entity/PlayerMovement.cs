4dbbfcaf450ddc9b9d6ee2d9ad70ef4ed7d0245f
suho1210 <tngh1313@gmail.com>
2025-07-25 19:12:55 +0900

    player,main controller

--- Assets/Scripts/Entity/PlayerMovement.cs
+++ Assets/Scripts/Entity/PlayerMovement.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PllayerMovement : MonoBehaviour
+public class PlayerController : BaseController
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera camera;
+
+    protected override void Start()
     {
-        
+        base.Start();
+        camera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void HandleAction()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertial = Input.GetAxisRaw("Vertical");
+        movementDirection = new Vector2(horizontal, vertial).normalized;
+
+        Vector2 mousePosition = Input.mousePosition;
+        Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
+        lookDirection = (worldPos - (Vector2)transform.position);
+
+        if (lookDirection.magnitude < .9f)
+        {
+            lookDirection = Vector2.zero;
+        }
+        else
+        {
+            lookDirection = lookDirection.normalized;
+        }
     }
 }
